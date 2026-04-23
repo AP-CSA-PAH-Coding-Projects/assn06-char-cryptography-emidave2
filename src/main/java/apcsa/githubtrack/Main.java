777e6dbe0b55d8ee50f2bd7d2f72d0d1819ce44d
Emilia <won.in.emilia.n@gmail.com>
2026-04-23 23:34:25 +0200

    A useful message...

--- src/main/java/apcsa/githubtrack/Main.java
+++ src/main/java/apcsa/githubtrack/Main.java
@@ -1,3 +1,9 @@
 package apcsa.githubtrack;
 
 // Implement your main application logic here
+public class Main {
+  public static void main(String[] args) {
+    CString cString = CStringUtil.toCString("Hello, World!");
+    System.out.println(CStringUtil.fromCString(cString));
+  }
+}
